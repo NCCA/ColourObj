ed8029cef4a62d87fdd986bc19921f7cee46549f
jmacey <jmacey@bournemouth.ac.uk>
2022-11-07 12:22:50 +0000

    updated for NGL9 master branch changed to main

--- src/NGLScene.cpp
+++ src/NGLScene.cpp
@@ -7,174 +7,166 @@
 #include <ngl/ShaderLib.h>
 #include <ngl/VAOPrimitives.h>
 
-
 NGLScene::NGLScene()
 {
-  setTitle( "Qt5 Simple NGL Demo" );
+  setTitle("Qt5 Simple NGL Demo");
 }
-
 
 NGLScene::~NGLScene()
 {
   std::cout << "Shutting down NGL, removing VAO's and Shaders\n";
 }
 
-
-
-void NGLScene::resizeGL( int _w, int _h )
+void NGLScene::resizeGL(int _w, int _h)
 {
-  m_project=ngl::perspective( 45.0f, static_cast<float>( _w ) / _h, 0.05f, 350.0f );
-  m_win.width  = static_cast<int>( _w * devicePixelRatio() );
-  m_win.height = static_cast<int>( _h * devicePixelRatio() );
+  m_project = ngl::perspective(45.0f, static_cast<float>(_w) / _h, 0.05f, 350.0f);
+  m_win.width = static_cast<int>(_w * devicePixelRatio());
+  m_win.height = static_cast<int>(_h * devicePixelRatio());
 }
 constexpr auto shaderProgram = "Colour";
-
 
 void NGLScene::initializeGL()
 {
   // we must call that first before any other GL commands to load and link the
   // gl commands from the lib, if that is not done program will crash
-  ngl::NGLInit::instance();
-  glClearColor( 0.4f, 0.4f, 0.4f, 1.0f ); // Grey Background
+  ngl::NGLInit::initialize();
+  glClearColor(0.4f, 0.4f, 0.4f, 1.0f); // Grey Background
   // enable depth testing for drawing
-  glEnable( GL_DEPTH_TEST );
+  glEnable(GL_DEPTH_TEST);
 // enable multisampling for smoother drawing
 #ifndef USINGIOS_
-  glEnable( GL_MULTISAMPLE );
+  glEnable(GL_MULTISAMPLE);
 #endif
   // now to load the shader and set the values
-  // grab an instance of shader manager
-  ngl::ShaderLib* shader = ngl::ShaderLib::instance();
-  shader->loadShader(shaderProgram,"shaders/ColourVertex.glsl","shaders/ColourFragment.glsl");
+  ngl::ShaderLib::loadShader(shaderProgram, "shaders/ColourVertex.glsl", "shaders/ColourFragment.glsl");
   // and make it active ready to load values
-  ( *shader )[ shaderProgram ]->use();
+  ngl::ShaderLib::use(shaderProgram);
   // First create Values for the camera position
-  ngl::Vec3 from( 0, 20, 20 );
-  ngl::Vec3 to( 0, 0, 0 );
-  ngl::Vec3 up( 0, 1, 0 );
+  ngl::Vec3 from(0, 2, 2);
+  ngl::Vec3 to(0, 0, 0);
+  ngl::Vec3 up(0, 1, 0);
   // now load to our new camera
-  m_view=ngl::lookAt( from, to, up );
+  m_view = ngl::lookAt(from, to, up);
   // set the shape using FOV 45 Aspect Ratio based on Width and Height
   // The final two are near and far clipping planes of 0.5 and 10
-  m_project=ngl::perspective( 45.0f, 720.0f / 576.0f, 0.05f, 350.0f );
-  m_mesh.reset(  new ColourObj("models/faceOrigin.obj"));
+  m_project = ngl::perspective(45.0f, 720.0f / 576.0f, 0.05f, 350.0f);
+  m_mesh.reset(new ColourObj("models/face_mesh_neutral.obj"));
   // now we need to create this as a VAO so we can draw it
   m_mesh->createColourVAO();
-
 }
-
 
 void NGLScene::loadMatricesToShader()
 {
-  ngl::ShaderLib* shader = ngl::ShaderLib::instance();
 
-  shader->setUniform( "MVP",m_project*m_view* m_mouseGlobalTX );
+  ngl::ShaderLib::setUniform("MVP", m_project * m_view * m_mouseGlobalTX);
 }
 
 void NGLScene::paintGL()
 {
-  glViewport( 0, 0, m_win.width, m_win.height );
+  glViewport(0, 0, m_win.width, m_win.height);
   // clear the screen and depth buffer
-  glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-  // grab an instance of the shader manager
-  ngl::ShaderLib* shader = ngl::ShaderLib::instance();
-  ( *shader )[ shaderProgram ]->use();
-
+  ngl::ShaderLib::use(shaderProgram);
   // Rotation based on the mouse position for our global transform
-  ngl::Mat4 rotX;
-  ngl::Mat4 rotY;
-  // create the rotation matrices
-  rotX.rotateX( m_win.spinXFace );
-  rotY.rotateY( m_win.spinYFace );
+  auto rotX = ngl::Mat4::rotateX(m_win.spinXFace);
+  auto rotY = ngl::Mat4::rotateY(m_win.spinYFace);
   // multiply the rotations
   m_mouseGlobalTX = rotY * rotX;
   // add the translations
-  m_mouseGlobalTX.m_m[ 3 ][ 0 ] = m_modelPos.m_x;
-  m_mouseGlobalTX.m_m[ 3 ][ 1 ] = m_modelPos.m_y;
-  m_mouseGlobalTX.m_m[ 3 ][ 2 ] = m_modelPos.m_z;
+  m_mouseGlobalTX.m_m[3][0] = m_modelPos.m_x;
+  m_mouseGlobalTX.m_m[3][1] = m_modelPos.m_y;
+  m_mouseGlobalTX.m_m[3][2] = m_modelPos.m_z;
 
   loadMatricesToShader();
   m_mesh->draw();
-
 }
 
 //----------------------------------------------------------------------------------------------------------------------
-void NGLScene::mouseMoveEvent( QMouseEvent* _event )
+void NGLScene::mouseMoveEvent(QMouseEvent *_event)
 {
-  // note the method buttons() is the button state when event was called
-  // that is different from button() which is used to check which button was
-  // pressed when the mousePress/Release event is generated
-  if ( m_win.rotate && _event->buttons() == Qt::LeftButton )
+// note the method buttons() is the button state when event was called
+// that is different from button() which is used to check which button was
+// pressed when the mousePress/Release event is generated
+#if QT_VERSION > QT_VERSION_CHECK(6, 0, 0)
+  auto position = _event->position();
+#else
+  auto position = _event->pos();
+#endif
+  if (m_win.rotate && _event->buttons() == Qt::LeftButton)
   {
-    int diffx = _event->x() - m_win.origX;
-    int diffy = _event->y() - m_win.origY;
-    m_win.spinXFace += static_cast<int>( 0.5f * diffy );
-    m_win.spinYFace += static_cast<int>( 0.5f * diffx );
-    m_win.origX = _event->x();
-    m_win.origY = _event->y();
+    int diffx = position.x() - m_win.origX;
+    int diffy = position.y() - m_win.origY;
+    m_win.spinXFace += static_cast<int>(0.5f * diffy);
+    m_win.spinYFace += static_cast<int>(0.5f * diffx);
+    m_win.origX = position.x();
+    m_win.origY = position.y();
     update();
   }
   // right mouse translate code
-  else if ( m_win.translate && _event->buttons() == Qt::RightButton )
+  else if (m_win.translate && _event->buttons() == Qt::RightButton)
   {
-    int diffX      = static_cast<int>( _event->x() - m_win.origXPos );
-    int diffY      = static_cast<int>( _event->y() - m_win.origYPos );
-    m_win.origXPos = _event->x();
-    m_win.origYPos = _event->y();
+    int diffX = static_cast<int>(position.x() - m_win.origXPos);
+    int diffY = static_cast<int>(position.y() - m_win.origYPos);
+    m_win.origXPos = position.x();
+    m_win.origYPos = position.y();
     m_modelPos.m_x += INCREMENT * diffX;
     m_modelPos.m_y -= INCREMENT * diffY;
     update();
   }
 }
 
-
 //----------------------------------------------------------------------------------------------------------------------
-void NGLScene::mousePressEvent( QMouseEvent* _event )
+void NGLScene::mousePressEvent(QMouseEvent *_event)
 {
-  // that method is called when the mouse button is pressed in this case we
-  // store the value where the maouse was clicked (x,y) and set the Rotate flag to true
-  if ( _event->button() == Qt::LeftButton )
+// that method is called when the mouse button is pressed in this case we
+// store the value where the maouse was clicked (x,y) and set the Rotate flag to true
+#if QT_VERSION > QT_VERSION_CHECK(6, 0, 0)
+  auto position = _event->position();
+#else
+  auto position = _event->pos();
+#endif
+  if (_event->button() == Qt::LeftButton)
   {
-    m_win.origX  = _event->x();
-    m_win.origY  = _event->y();
+    m_win.origX = position.x();
+    m_win.origY = position.y();
     m_win.rotate = true;
   }
   // right mouse translate mode
-  else if ( _event->button() == Qt::RightButton )
+  else if (_event->button() == Qt::RightButton)
   {
-    m_win.origXPos  = _event->x();
-    m_win.origYPos  = _event->y();
+    m_win.origXPos = position.x();
+    m_win.origYPos = position.y();
     m_win.translate = true;
   }
 }
 
 //----------------------------------------------------------------------------------------------------------------------
-void NGLScene::mouseReleaseEvent( QMouseEvent* _event )
+void NGLScene::mouseReleaseEvent(QMouseEvent *_event)
 {
   // that event is called when the mouse button is released
   // we then set Rotate to false
-  if ( _event->button() == Qt::LeftButton )
+  if (_event->button() == Qt::LeftButton)
   {
     m_win.rotate = false;
   }
   // right mouse translate mode
-  if ( _event->button() == Qt::RightButton )
+  if (_event->button() == Qt::RightButton)
   {
     m_win.translate = false;
   }
 }
 
 //----------------------------------------------------------------------------------------------------------------------
-void NGLScene::wheelEvent( QWheelEvent* _event )
+void NGLScene::wheelEvent(QWheelEvent *_event)
 {
 
   // check the diff of the wheel position (0 means no change)
-  if ( _event->delta() > 0 )
+  if (_event->angleDelta().y() > 0)
   {
     m_modelPos.m_z += ZOOM;
   }
-  else if ( _event->delta() < 0 )
+  else if (_event->angleDelta().y() < 0)
   {
     m_modelPos.m_z -= ZOOM;
   }
@@ -182,36 +174,36 @@ void NGLScene::wheelEvent( QWheelEvent* _event )
 }
 //----------------------------------------------------------------------------------------------------------------------
 
-void NGLScene::keyPressEvent( QKeyEvent* _event )
+void NGLScene::keyPressEvent(QKeyEvent *_event)
 {
   // that method is called every time the main window recives a key event.
   // we then switch on the key value and set the camera in the GLWindow
-  switch ( _event->key() )
+  switch (_event->key())
   {
-    // escape key to quit
-    case Qt::Key_Escape:
-      QGuiApplication::exit( EXIT_SUCCESS );
-      break;
+  // escape key to quit
+  case Qt::Key_Escape:
+    QGuiApplication::exit(EXIT_SUCCESS);
+    break;
 // turn on wirframe rendering
 #ifndef USINGIOS_
-    case Qt::Key_W:
-      glPolygonMode( GL_FRONT_AND_BACK, GL_LINE );
-      break;
-    // turn off wire frame
-    case Qt::Key_S:
-      glPolygonMode( GL_FRONT_AND_BACK, GL_FILL );
-      break;
+  case Qt::Key_W:
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    break;
+  // turn off wire frame
+  case Qt::Key_S:
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    break;
 #endif
-    // show full screen
-    case Qt::Key_F:
-      showFullScreen();
-      break;
-    // show windowed
-    case Qt::Key_N:
-      showNormal();
-      break;
-    default:
-      break;
+  // show full screen
+  case Qt::Key_F:
+    showFullScreen();
+    break;
+  // show windowed
+  case Qt::Key_N:
+    showNormal();
+    break;
+  default:
+    break;
   }
   update();
 }
